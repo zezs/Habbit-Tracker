7dfef3556805d33f805be061116eb65c2143978e
zezs <my.version.ctrl@gmail.com>
2024-05-02 02:08:21 +0200

    Displaying and saving habits

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template
 import os
 
 app =Flask(__name__)
+habits = ["Test habit"]
 
 @app.route("/")
-def hello():
-    return "Hello ji!"
+def index():
+    return render_template("index.html", title="Habit Tracker - Home", habits=habits)
+
+@app.route("/add", methods=["GET", "POST"])
+def add_habit():
+    return render_template("add_habit.html", title="Habit Tracker - Add Habit")
