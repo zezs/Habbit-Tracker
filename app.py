0a2779a04a4c6aef6f000529ef57424cc0f78070
zezs <my.version.ctrl@gmail.com>
2024-05-03 15:52:40 +0200

    completing habit and fixing style sheet

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template, request
-import os
 import datetime
+from collections import defaultdict
+from flask import Flask, render_template, request, redirect, url_for
 
-app =Flask(__name__)
+app = Flask(__name__)
 habits = ["Test habit"]
+completions = defaultdict(list)
+
 
 @app.context_processor
 def add_calc_date_range():
     def date_range(start: datetime.date):
         dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
         return dates
-    
+
     return {"date_range": date_range}
+
 
 @app.route("/")
 def index():
@@ -21,12 +24,32 @@ def index():
     else:
         selected_date = datetime.date.today()
 
-    return render_template("index.html", title="Habit Tracker - Home", habits=habits, selected_date=selected_date)
+    return render_template(
+        "index.html",
+        habits=habits,
+        selected_date=selected_date,
+        completions=completions[selected_date],
+        title="Habit Tracker - Home",
+    )
+
+
+@app.route("/complete", methods=["POST"])
+def complete():
+    date_string = request.form.get("date")
+    date = datetime.date.fromisoformat(date_string)
+    habit = request.form.get("habitName")
+    completions[date].append(habit)
+
+    return redirect(url_for("index", date=date_string))
+
 
 @app.route("/add", methods=["GET", "POST"])
 def add_habit():
-    if request.method == "POST":
-        habit = request.form.get("habit")
-        habits.append(habit)
+    if request.form:
+        habits.append(request.form.get("habit"))
 
-    return render_template("add_habit.html", title="Habit Tracker - Add Habit", selected_date=datetime.date.today())
+    return render_template(
+        "add_habit.html",
+        title="Habit Tracker - Add Habit",
+        selected_date=datetime.date.today(),
+    )
