ea0ab95ee4c39877e78bfca4344527046408c381
zezs <my.version.ctrl@gmail.com>
2024-05-03 04:14:25 +0200

    Adding the date navigation

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template, request
 import os
+import datetime
 
 app =Flask(__name__)
 habits = ["Test habit"]
 
+@app.context_processor
+def add_calc_date_range():
+    def date_range(start: datetime.date):
+        dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
+        return dates
+    
+    return {"date_range": date_range}
+
 @app.route("/")
 def index():
-    return render_template("index.html", title="Habit Tracker - Home", habits=habits)
+    date_str = request.args.get("date")
+    if date_str:
+        selected_date = datetime.date.fromisoformat(date_str)
+    else:
+        selected_date = datetime.date.today()
+
+    return render_template("index.html", title="Habit Tracker - Home", habits=habits, selected_date=selected_date)
 
 @app.route("/add", methods=["GET", "POST"])
 def add_habit():
@@ -14,4 +29,4 @@ def add_habit():
         habit = request.form.get("habit")
         habits.append(habit)
 
-    return render_template("add_habit.html", title="Habit Tracker - Add Habit")
+    return render_template("add_habit.html", title="Habit Tracker - Add Habit", selected_date=datetime.date.today())
